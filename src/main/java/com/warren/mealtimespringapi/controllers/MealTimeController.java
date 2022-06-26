427cb5db6cff1f159f305bfbac02e85c1621b1a5
Warren Chu <76016561+warrenchu@users.noreply.github.com>
2022-06-25 23:00:54 -0400

    Controller is contacting the API correctly

--- src/main/java/com/warren/mealtimespringapi/controllers/MealTimeController.java
+++ src/main/java/com/warren/mealtimespringapi/controllers/MealTimeController.java
@@ -2,9 +2,7 @@ package com.warren.mealtimespringapi.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -13,19 +11,23 @@ import java.net.URI;
 import java.net.http.HttpRequest;
 
 @RestController
+@CrossOrigin(origins="*")
 public class MealTimeController {
     @Value("${api.key}")
     private String apiKey;
     @Value("${api.address}")
     private String api_Address;
 
-    @GetMapping("/randomInCity/{cityName}/{price}")
-    public ResponseEntity<?> getRandomRestaurantInCity(@PathVariable String cityName, @PathVariable int price){
+    @GetMapping("/restaurants")
+    public ResponseEntity<?> getRandomRestaurantInCity(@RequestParam(name="cityName") String cityName, @RequestParam (name="price") int price,
+                                                       @RequestParam(name="cuisine") String cuisineType, @RequestParam(name="limit") int limit){
         RestTemplate restTemplate = new RestTemplate();
         String uri = UriComponentsBuilder.fromHttpUrl(api_Address)
                 .queryParam("location", cityName)
-                .queryParam("limit", 5)
+                .queryParam("limit", limit)
+                .queryParam("term",  cuisineType)
                 .queryParam("price", price)
+                .queryParam("radius", 15000)
                 .encode()
                 .toUriString();
         HttpHeaders headers = new HttpHeaders();
